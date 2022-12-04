388c0e3c3ff8799ace54a1769d793380ecc34ceb
arafimam -Git <syedarafimam27@gmail.com>
2022-12-03 18:36:25 -0800

    removed main

--- src/main/java/arrays/ContainsDuplicate.java
+++ src/main/java/arrays/ContainsDuplicate.java
@@ -5,10 +5,6 @@ import java.util.Set;
 
 public class ContainsDuplicate {
 
-    public static void main(String[] args) {
-        System.out.println(containsDuplicate(new int[]{1, 2, 3, 4}));
-    }
-
     public static boolean containsDuplicate(int[] nums) {
 
         Set<Integer> uniqueElements = new HashSet<>();
