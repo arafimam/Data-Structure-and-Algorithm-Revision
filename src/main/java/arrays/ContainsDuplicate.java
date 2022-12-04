aa9a9c36669fff8e6e7544bf7ccd0b0e7eb88319
arafimam -Git <syedarafimam27@gmail.com>
2022-12-03 18:33:20 -0800

    contains Duplicate Problem solved

---
+++ src/main/java/arrays/ContainsDuplicate.java
@@ -0,0 +1,25 @@
+package arrays;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ContainsDuplicate {
+
+    public static void main(String[] args) {
+        System.out.println(containsDuplicate(new int[]{1, 2, 3, 4}));
+    }
+
+    public static boolean containsDuplicate(int[] nums) {
+
+        Set<Integer> uniqueElements = new HashSet<>();
+        for (int i = 0; i < nums.length; i ++){
+            if (!uniqueElements.contains(nums[i])){
+                uniqueElements.add(nums[i]);
+            }else {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
